a22e038d1257c82891a028b5a9556a978c0a10dd
JosePedrozo <j.alfredopedrozo@gmail.com>
2026-03-24 16:15:29 -0300

    - ajustes backend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const postRoutes = require('./routes/post.routes');
+const cors = require('cors'); 
 
 const app = express();
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
 app.use(express.json());
 
+const postRoutes = require('./routes/post.routes');
 app.use('/posts', postRoutes);
 
 module.exports = app;
